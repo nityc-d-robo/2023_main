83697bf5f1ff88c2ab0900237f0b3c416302f69a
sarukiti <sarukiti1891@gmail.com>
2023-08-24 22:35:58 +0900

    add: motor_lib用ROSラッパーを追加

---
+++ src/dmotor_ros/src/main.cpp
@@ -0,0 +1,52 @@
+#include <rclcpp/rclcpp.hpp>
+#include <rclcpp/qos.hpp>
+#include <drobo_interfaces/msg/md_lib_msg.hpp>
+#include <drobo_interfaces/msg/sd_lib_msg.hpp>
+#include <motor_lib/motor_lib.hpp>
+
+#include "dmotor_ros/main.hpp"
+
+DMotorRos::DMotorRos(
+    const rclcpp::NodeOptions& options
+): DMotorRos("", options){}
+
+DMotorRos::DMotorRos(
+    const std::string& name_space,
+    const rclcpp::NodeOptions& options
+): Node("d_motor_ros", name_space, options){
+    _subscription_md = this->create_subscription<drobo_interfaces::msg::MdLibMsg>(
+        "md_driver_topic",
+        rclcpp::QoS(10),
+        [this](const drobo_interfaces::msg::MdLibMsg::SharedPtr msg){
+            switch(msg->mode){
+                case MotorLib::Md::PWM:
+                    MotorLib::md.sendPwm(msg->address, msg->semi_id, msg->phase, msg->power, 5000);
+                    break;
+                case MotorLib::Md::SPEED:
+                    MotorLib::md.sendSpeed(msg->address, msg->semi_id, msg->phase, msg->power, 0, 1000, 5000);
+                    break;
+            }
+        }
+    );
+    _subscription_sd = this->create_subscription<drobo_interfaces::msg::SdLibMsg>(
+        "sd_driver_topic",
+        rclcpp::QoS(10),
+        [this](const drobo_interfaces::msg::SdLibMsg::SharedPtr msg){
+            MotorLib::sd.sendPowers(msg->address, msg->semi_id, msg->power1, msg->power2, 5000);
+        }
+    );
+}
+
+int main(int argc, char* argv[]){
+    MotorLib::usb_config.vendor_id = 0x483;
+    MotorLib::usb_config.product_id = 0x5740;
+    MotorLib::usb_config.b_interface_number = 0;
+
+    MotorLib::usb.setUsb(&MotorLib::usb_config);
+    MotorLib::usb.openUsb();
+
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<DMotorRos>());
+    rclcpp::shutdown();
+    return 0;
+}
